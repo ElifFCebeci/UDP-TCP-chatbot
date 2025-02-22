52d5fbff64501547a0d872f74f08790233801932
ElifFCebeci <153857092+ElifFCebeci@users.noreply.github.com>
2025-02-22 14:24:22 +0300

    Update Client.go

--- Client.go
+++ Client.go
@@ -1,3 +1,4 @@
+//UDP-client.go
 package main
 
 import (
@@ -6,18 +7,19 @@ import (
 )
 
 func main() {
+	//adres tanımlanması
 	serverAddr := net.UDPAddr{
 		Port: 12345,
 		IP:   net.ParseIP("127.0.0.1"),
 	}
-
+       //adrese bağlantı açılması
 	conn, err := net.DialUDP("udp", nil, &serverAddr)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
-
+      // veri okunması
 	go func() {
 		buffer := make([]byte, 1024)
 		for {
@@ -29,7 +31,7 @@ func main() {
 			fmt.Printf("Received: %s\n", string(buffer[:n]))
 		}
 	}()
-
+       //kullanıcıdan mesaj alınması
 	for {
 		var msg string
 		fmt.Print("Mesaj girin: ")
